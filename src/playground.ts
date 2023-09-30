ac9f4434269e01686ecd3e06e65c440bbc16e232
Vincent Liao <vincent.liao@protonmail.com>
2023-09-30 08:29:25 +0700

    add: biomejs

---
+++ src/playground.ts
@@ -0,0 +1,4 @@
+import { warpcast } from "./poster";
+
+const poster = warpcast("my-api-key");
+poster("hi");
--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,19 @@
 export const clog = (description: string, data: any): void => {
-  const appendFile = async (path: string, data: string) => {
-    const file = Bun.file(path);
-    const log = (await file.text()) + data + "\n";
-    await Bun.write(path, log);
-  };
+	const appendFile = async (path: string, data: string) => {
+		const file = Bun.file(path);
+		const log = `${await file.text()}${data}\n`;
+		await Bun.write(path, log);
+	};
 
-  const now = new Date();
-  const timestamp = now.toISOString();
-  const log = `${timestamp} - ${description} - ${
-    data instanceof Object ? JSON.stringify(data, null, 2) : data
-  }\n`;
+	const now = new Date();
+	const timestamp = now.toISOString();
+	const log = `${timestamp} - ${description} - ${
+		data instanceof Object ? JSON.stringify(data, null, 2) : data
+	}\n`;
 
-  console.log(log);
-  appendFile("./app.log", log);
+	console.log(log);
+	appendFile("./app.log", log);
 };
 
 export const sleep = (ms: number): Promise<void> =>
-  new Promise((resolve) => setTimeout(resolve, ms));
+	new Promise((resolve) => setTimeout(resolve, ms));
